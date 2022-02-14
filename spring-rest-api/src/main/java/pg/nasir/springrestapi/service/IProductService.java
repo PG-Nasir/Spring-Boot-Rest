3197185267d477dc376150be7585635856fe103b
Nasir <pgnasir17@gmail.com>
2022-02-14 22:39:37 +0600

    first commit

---
+++ spring-rest-api/src/main/java/pg/nasir/springrestapi/service/IProductService.java
@@ -0,0 +1,10 @@
+package pg.nasir.springrestapi.service;
+
+import java.util.List;
+
+import pg.nasir.springrestapi.model.Product;
+
+public interface IProductService   
+{  
+	List<Product> findAll();  
+}  
